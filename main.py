2ed84e2003f52a72e2c886967944d25bfd250b07
Atte Jokinen <atte.jokinen@tuni.fi>
2020-01-27 19:26:32 +0200

    Recognizes faces now

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import cv2
 import numpy as np
 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
 cap = cv2.VideoCapture(0)
 
 while(True):
-    # Capture frame-by-frame
+
     ret, frame = cap.read()
-
-    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x,y,w,h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y + h, x:x + w]
+        
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
 
-    # Display the resulting frame
-    cv2.imshow('webcam',gray)
+        color = (255,0,0) #BGR
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
+
+    cv2.imshow('webcam',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
